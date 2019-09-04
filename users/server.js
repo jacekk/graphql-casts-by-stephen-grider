d048d7e2a40d1fbc50ce5162bf6ffe7d45a26f6c
jacekk <jacek.kapias@amsterdamstandard.com>
2019-09-04 15:27:19 +0200

    added "formatOnSave" into settings;

--- users/server.js
+++ users/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 
+const { schema } = require('./schema')
+
 const app = express()
 
-app.use('/query', expressGraphQL({
-  graphiql: true
-}));
+app.use(
+	'/query',
+	expressGraphQL({
+		graphiql: true,
+		schema,
+	})
+)
 
 app.listen(4000, () => {
 	console.log('App is listening on port 4000 ...')
