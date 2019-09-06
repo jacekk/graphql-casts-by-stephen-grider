7b29f0eb0ede98b95673aadb18b3f7ca037e3c8e
jacekk <jacek.kapias@amsterdamstandard.com>
2019-09-06 09:32:08 +0200

    added remove user btn;

--- users-client/src/components/UsersList.js
+++ users-client/src/components/UsersList.js
@@ -5,8 +5,13 @@ import './UsersList.sass'
 export const UsersList = (props) => (
 	<ul className="list-group users-list__wrapper">
 		{props.users.map((user) => (
-			<li key={user.id} className="list-group-item">
-				{user.id} | {user.name}
+			<li key={user.id} className="list-group-item d-flex justify-content-between">
+				<span className="users-list__list-item-details d-flex align-items-center">
+					{user.id} | {user.name}
+				</span>
+				<button type="button" class="btn btn-danger btn-sm" onClick={props.onRemoveClick}>
+					✖
+				</button>
 			</li>
 		))}
 	</ul>
